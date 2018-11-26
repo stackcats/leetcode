a7679970eec96aae83af63ca8261cefab2aa93bb
stackcats <stackcats@foxmail.com>
2018-11-26 20:46:57 +0800

    merge_two_binary_trees.c

---
+++ merge_two_binary_trees.c
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/merge-two-binary-trees/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode *mergeTrees(struct TreeNode *t1, struct TreeNode *t2) {
+  if (!t1 && !t2) {
+    return NULL;
+  }
+
+  if (!t1) {
+    return t2;
+  }
+
+  if (!t2) {
+    return t1;
+  }
+
+  t1->val += t2->val;
+  t1->left = mergeTrees(t1->left, t2->left);
+  t1->right = mergeTrees(t1->right, t2->right);
+  return t1;
+}
