31392792c82adae3d85cc872bf0fcb6f7f41a9a3
stackcats <stackcats@foxmail.com>
2018-11-27 11:16:03 +0800

    maximum_depth_of_n_ary_tree.py

---
+++ maximum_depth_of_n_ary_tree.py
@@ -0,0 +1,34 @@
+#  https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+
+# 层次遍历实现非递归
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        res = 0
+
+        q = [(root, 1)]
+
+        while len(q) > 0:
+            node, depth = q.pop(0)
+            if res < depth:
+                res = depth
+
+            for c in node.children:
+                q.append((c, depth + 1))
+
+        return res
+            
+        
