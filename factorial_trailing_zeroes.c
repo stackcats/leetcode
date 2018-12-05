5c6d0adc2345d2f1074aa8fe41a519a9df795c28
stackcats <stackcats@foxmail.com>
2018-12-05 18:06:12 +0800

    factorial_trailing_zeroes.c

---
+++ factorial_trailing_zeroes.c
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/factorial-trailing-zeroes/description/
+
+int trailingZeroes(int n) {
+  int ans = 0;
+  while (n > 0) {
+    ans += n / 5;
+    n /= 5;
+  }
+  return ans;
+}
