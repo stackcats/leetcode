bc0cb08da54b6d10dba312e3ecbf6529a2b65245
stackcats <stackcats@foxmail.com>
2018-12-09 11:03:47 +0800

    iterate verson

--- insert_into_a_binary_search_tree.c
+++ insert_into_a_binary_search_tree.c
@@ -10,17 +10,33 @@
  */
 typedef struct TreeNode TreeNode;
 struct TreeNode *insertIntoBST(struct TreeNode *root, int val) {
-  if (!root) {
-    TreeNode *t = malloc(sizeof(TreeNode));
-    t->val = val;
-    t->right = t->left = NULL;
+  TreeNode *parnet = NULL;
+  TreeNode *iter = root;
+  int isLeft = 1;
+  while (iter) {
+    if (iter->val > val) {
+      parnet = iter;
+      iter = iter->left;
+      isLeft = 1;
+    } else {
+      parnet = iter;
+      iter = iter->right;
+      isLeft = 0;
+    }
+  }
+
+  TreeNode *t = malloc(sizeof(TreeNode));
+  t->val = val;
+  t->right = t->left = NULL;
+  if (parnet == NULL) {
     return t;
   }
 
-  if (root->val > val) {
-    root->left = insertIntoBST(root->left, val);
+  if (isLeft) {
+    parnet->left = t;
   } else {
-    root->right = insertIntoBST(root->right, val);
+    parnet->right = t;
   }
+
   return root;
 }
