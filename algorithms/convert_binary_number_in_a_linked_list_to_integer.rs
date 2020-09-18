1f1d67e43f36761749aded2a6d93be3359198ec2
stackcats <stackcats@foxmail.com>
2020-09-18 13:42:03 +0800

    algorithms/convert_binary_number_in_a_linked_list_to_integer.rs

---
+++ algorithms/convert_binary_number_in_a_linked_list_to_integer.rs
@@ -0,0 +1,27 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn get_decimal_value(mut head: Option<Box<ListNode>>) -> i32 {
+        let mut ans = 0;
+        while head.is_some() {
+            let h = head.unwrap();
+            ans = ans * 2 + h.val;
+            head = h.next;
+        }
+        ans
+    }
+}
