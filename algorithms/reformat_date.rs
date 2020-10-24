0912d929386cf3b58b702d9a59665902016b8e87
stackcats <stackcats@foxmail.com>
2020-10-24 21:51:49 +0800

    algorithms/reformat_date.rs

---
+++ algorithms/reformat_date.rs
@@ -0,0 +1,20 @@
+macro_rules! map(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+                m
+        }
+    };
+);
+
+impl Solution {
+    pub fn reformat_date(date: String) -> String {
+        let day = map!["1st" => "01", "2nd"=> "02", "3rd"=> "03", "4th"=> "04", "5th"=> "05", "6th"=> "06","7th"=> "07","8th" => "08","9th"=> "09","10th" => "10","11th" => "11","12th"=> "12","13th" => "13","14th" => "14","15th" => "15","16th"=> "16","17th"=> "17","18th"=> "18","19th"=> "19","20th"=> "20","21st"=> "21","22nd"=> "22","23rd"=> "23","24th"=> "24","25th"=> "25","26th"=> "26","27th"=> "27","28th"=> "28","29th"=> "29","30th" => "30", "31st" => "31"];
+        let month = map!["Jan" => "01", "Feb" => "02", "Mar" => "03", "Apr" => "04", "May" => "05", "Jun" => "06", "Jul" => "07", "Aug" => "08", "Sep" => "09", "Oct" => "10", "Nov" => "11", "Dec" => "12"];
+        let arr = date.split(" ").collect::<Vec<&str>>();
+        format!("{}-{}-{}", arr[2], month[arr[1]], day[arr[0]])
+    }
+}
