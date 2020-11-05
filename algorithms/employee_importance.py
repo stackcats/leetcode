954d52b5aecd2a452a7bfd8e89ecf45f449ca6c4
stackcats <stackcats@foxmail.com>
2020-11-05 16:24:28 +0800

    algorithms/employee_importance.py

---
+++ algorithms/employee_importance.py
@@ -0,0 +1,28 @@
+"""
+# Definition for Employee.
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+    	#################
+        :type id: int
+        :type importance: int
+        :type subordinates: List[int]
+        #################
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: List[Employee]
+        :type id: int
+        :rtype: int
+        """
+        importance = 0
+        for e in employees:
+            if e.id == id:
+                importance += e.importance
+                importance += sum([self.getImportance(employees, s) for s in e.subordinates])
+                break
+        return importance
