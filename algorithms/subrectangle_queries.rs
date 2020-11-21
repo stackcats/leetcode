146138251d810894ddd7e257bf6e842522b61695
stackcats <stackcats@foxmail.com>
2020-11-21 10:57:48 +0800

    algorithms/subrectangle_queries.rs

---
+++ algorithms/subrectangle_queries.rs
@@ -0,0 +1,34 @@
+struct SubrectangleQueries {
+    mat: Vec<Vec<i32>>
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl SubrectangleQueries {
+
+    fn new(rectangle: Vec<Vec<i32>>) -> Self {
+        SubrectangleQueries { mat: rectangle }
+    }
+    
+    fn update_subrectangle(&mut self, row1: i32, col1: i32, row2: i32, col2: i32, new_value: i32) {
+        for i in row1..=row2 {
+            for j in col1..=col2 {
+                self.mat[i as usize][j as usize] = new_value;
+            }
+        }
+    }
+    
+    fn get_value(&self, row: i32, col: i32) -> i32 {
+        self.mat[row as usize][col as usize]
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * let obj = SubrectangleQueries::new(rectangle);
+ * obj.update_subrectangle(row1, col1, row2, col2, newValue);
+ * let ret_2: i32 = obj.get_value(row, col);
+ */
