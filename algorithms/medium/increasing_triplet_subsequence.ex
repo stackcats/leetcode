ad446ca2496598ae2972267b696d136d8197dd24
stackcats <stackcats@foxmail.com>
2021-09-20 11:47:21 +0800

    algorithms/medium/increasing_triplet_subsequence.ex

---
+++ algorithms/medium/increasing_triplet_subsequence.ex
@@ -0,0 +1,18 @@
+defmodule Solution do
+  @spec increasing_triplet(nums :: [integer]) :: boolean
+  def increasing_triplet(nums) do
+    low = 2147483647
+    mid = 2147483647
+    r(low, mid, nums)
+  end
+      
+  def r(low, mid, []), do: false
+  def r(low, mid, [head|rest]) do
+    cond do
+        low > head -> r(head, mid, rest)
+        head > mid -> true
+        head > low and head < mid -> r(low, head, rest)
+        true -> r(low, mid, rest)
+    end
+  end
+end
