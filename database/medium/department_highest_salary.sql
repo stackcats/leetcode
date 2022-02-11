a066837f210fb6830e943ae918a92d6aa0137658
stackcats <stackcats@foxmail.com>
2022-02-11 22:42:20 +0800

    database/medium/department_highest_salary.sql

---
+++ database/medium/department_highest_salary.sql
@@ -0,0 +1,13 @@
+with cte as
+  (select e.name Employee,
+          e.salary Salary,
+          d.name Department,
+          rank() over(partition by d.name
+                      order by e.salary desc) rnk
+   from Employee e
+   left join Department d on e.departmentId = d.id)
+select Department,
+       Employee,
+       Salary
+from cte
+where rnk = 1
