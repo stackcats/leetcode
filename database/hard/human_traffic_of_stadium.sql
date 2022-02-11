4f13efa3b83d49aaaef7e62d120c2b41df0ceabd
stackcats <stackcats@foxmail.com>
2022-02-11 23:24:45 +0800

    database/hard/human_traffic_of_stadium.sql

--- database/hard/human_traffic_of_stadium.sql
+++ database/hard/human_traffic_of_stadium.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-
+with cte(startId, endId) as
   (SELECT L1.id,
-          L1.visit_date,
-          L1.people
+          L3.id
    FROM Stadium L1,
         Stadium L2,
         Stadium L3
@@ -11,28 +10,9 @@
      AND L1.people >= 100
      AND L2.people >= 100
      AND L3.people >= 100)
-union
-  (SELECT L2.id,
-          L2.visit_date,
-          L2.people
-   FROM Stadium L1,
-        Stadium L2,
-        Stadium L3
-   WHERE L3.id = L2.id + 1
-     AND L2.id = L1.id + 1
-     AND L1.people >= 100
-     AND L2.people >= 100
-     AND L3.people >= 100)
-union
-  (SELECT L3.id,
-          L3.visit_date,
-          L3.people
-   FROM Stadium L1,
-        Stadium L2,
-        Stadium L3
-   WHERE L3.id = L2.id + 1
-     AND L2.id = L1.id + 1
-     AND L1.people >= 100
-     AND L2.people >= 100
-     AND L3.people >= 100)
-order by id
+select distinct id,
+                visit_date,
+                people
+from Stadium,
+     cte
+where id between startId and endId
