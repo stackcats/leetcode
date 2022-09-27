d41b6b99cf7fdae2cf23c94f898c7aa84959d812
stackcats <stackcats.hk@gmail.com>
2022-09-27 13:28:20 +0800

    algorithms/easy/find_subarrays_with_equal_sum.rs

---
+++ algorithms/easy/find_subarrays_with_equal_sum.rs
@@ -0,0 +1,15 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn find_subarrays(nums: Vec<i32>) -> bool {
+        let mut set = HashSet::new();
+        for i in 0..nums.len() - 1 {
+            let sum = nums[i] + nums[i + 1];
+            if set.contains(&sum) {
+                return true;
+            }
+            set.insert(sum);
+        }
+        false
+    }
+}
