91e3183979b2a53e4b1ba810bf9a10bd5ad3c243
stackcats <stackcats.hk@gmail.com>
2023-07-28 12:14:49 +0800

    javascript/easy/add_two_promises.ts

---
+++ javascript/easy/add_two_promises.ts
@@ -0,0 +1,10 @@
+async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
+    return new Promise(async (res) => {
+        return res(await promise1 + await promise2);
+    })
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
