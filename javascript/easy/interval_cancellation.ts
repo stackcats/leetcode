0a3adb6a7f475fbbecc88ae62a6004c533932cbe
stackcats <stackcats.hk@gmail.com>
2023-08-03 12:56:42 +0800

    javascript/easy/interval_cancellation.ts

---
+++ javascript/easy/interval_cancellation.ts
@@ -0,0 +1,36 @@
+function cancellable(fn: Function, args: any[], t: number): Function {
+    fn(...args);
+    const id = setInterval(fn, t, ...args);
+    return () => clearTimeout(id);
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start)
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *   
+ *  setTimeout(() => {
+ *    console.log(result)  // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},           
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT + t + 15)    
+ */
