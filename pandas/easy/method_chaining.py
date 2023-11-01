59e8429d763f48ca684d16f8d0d095840bbe2fad
stackcats <stackcats.hk@gmail.com>
2023-11-01 11:11:32 +0800

    pandas/easy/method_chaining.py

---
+++ pandas/easy/method_chaining.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals.query('weight > 100').sort_values('weight',
+                                                     ascending=False)[['name']]
