74e9ca1e51e9ce442b1757666879988df749192a
stackcats <stackcats.hk@gmail.com>
2024-07-19 15:58:43 +0800

    algorithms/easy/count_the_number_of_special_characters_i.rs

---
+++ algorithms/easy/count_the_number_of_special_characters_i.rs
@@ -0,0 +1,16 @@
+use std::iter::zip;
+
+impl Solution {
+    pub fn number_of_special_chars(word: String) -> i32 {
+        let mut lower = [false; 26];
+        let mut upper = [false; 26];
+        for c in word.as_bytes() {
+            if *c >= b'a' { 
+                lower[(c - b'a') as usize] = true;
+            } else { 
+                upper[(c - b'A') as usize] = true;
+            }
+        }
+        zip(lower, upper).filter(|(a, b)| *a && *b).count() as i32
+    }
+}
