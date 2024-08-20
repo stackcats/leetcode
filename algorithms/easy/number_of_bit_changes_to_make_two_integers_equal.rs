4e720eb4c0ade7c91334f95adf55103e5b90f282
stackcats <stackcats.hk@gmail.com>
2024-08-20 12:04:02 +0800

    algorithms/easy/number_of_bit_changes_to_make_two_integers_equal.rs

--- algorithms/easy/number_of_bit_changes_to_make_two_integers_equal.rs
+++ algorithms/easy/number_of_bit_changes_to_make_two_integers_equal.rs
@@ -1,11 +1,7 @@
 impl Solution {
     pub fn min_changes(n: i32, k: i32) -> i32 {
-        if n == k {
-            return 0;
-        }
-
         if n & k == k {
-            return (n.count_ones() - k.count_ones()) as i32
+            return (n ^ k).count_ones() as i32;
         }
 
         -1
