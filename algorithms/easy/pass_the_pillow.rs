8b330ed87ebb7b904ccbfe0ca0a142fe52be4994
stackcats <stackcats.hk@gmail.com>
2024-09-26 11:02:32 +0800

    pass_the_pillow.rs

--- algorithms/easy/pass_the_pillow.rs
+++ algorithms/easy/pass_the_pillow.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
-        let r = time / (n - 1);
-        let m = time % (n - 1);
-        if r % 2 == 0 {
-            m
+        let time = time % (n * 2 - 2);
+        if time < n - 1 {
+            time + 1
         } else {
-            n - m
+            n - (time - n) - 1
         }
     }
 }
+
