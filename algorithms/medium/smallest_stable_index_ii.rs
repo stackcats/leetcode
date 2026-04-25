3b95f1c3de0c95058603ccb9e763ba81d9c3a977
stackcats <stackcats.hk@gmail.com>
2026-04-25 12:35:23 +0800

    smallest_stable_index_ii.rs

---
+++ algorithms/medium/smallest_stable_index_ii.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn first_stable_index(nums: Vec<i32>, k: i32) -> i32 {
+        let mut arr = vec![0; nums.len()];
+        let mut mi = i32::MAX;
+        for i in (0..nums.len()).rev() {
+            mi = mi.min(nums[i]);
+            arr[i] = mi;
+        }
+
+        let mut ma = 0;
+        for i in 0..nums.len() {
+            ma = ma.max(nums[i]);
+            if ma - arr[i] <= k {
+                return i as _;
+            }
+        }
+
+        -1
+    }
+}
