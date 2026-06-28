82856b59ac581296d580770e72fc76da80039c4e
stackcats <stackcats.hk@gmail.com>
2026-06-28 11:09:26 +0800

    maximum_element_after_decreasing_and_rearranging.rs

---
+++ algorithms/medium/maximum_element_after_decreasing_and_rearranging.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn maximum_element_after_decrementing_and_rearranging(mut arr: Vec<i32>) -> i32 {
+        arr.sort();
+        arr[0] = 1;
+        for i in 1..arr.len() {
+            if (arr[i] - arr[i - 1]).abs() > 1 {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        arr[arr.len() - 1]
+    }
+}
