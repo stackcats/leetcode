e43a793900ef571f5da0ba19b2655af5d4cadf4b
stackcats <stackcats.hk@gmail.com>
2026-08-01 11:16:30 +0800

    bitwise_and_of_numbers_range.rs

---
+++ algorithms/medium/bitwise_and_of_numbers_range.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn range_bitwise_and(left: i32, mut right: i32) -> i32 {
+        while right > left {
+            right = right & (right - 1);
+        }
+        right & left
+    }
+}
